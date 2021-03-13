5f2a8a32febf03a7eb800af346c5a8128423f938
theselenaliu <79890458+theselenaliu@users.noreply.github.com>
2021-03-12 23:08:59 -0800

    Updated JS to include APIs and changed font color and positioning of weather details

--- src/index.js
+++ src/index.js
@@ -25,10 +25,42 @@ function searchResult(event) {
 	let cityElement = document.querySelector("#currentCity");
 	let cityInput = document.querySelector("#search");
 	cityElement.innerHTML = cityInput.value;
+	searchCity(cityInput);
+}
+
+function showTemperature(response) {
+	console.log("temp" + response.data.main.temp);
+	document.querySelector("#currentCity").innerHTML = response.data.name;
+	let temperature = Math.round(response.data.main.temp);
+	let temperatureElement = document.querySelector("#temperature");
+	temperatureElement.innerHTML = `${temperature}`;
+	let celsiusTemp = Math.round(temperature);
+	let fahrenheitTemp = Math.round((temperature * 9) / 5 + 32);
+}
+
+function searchCity(cityInput) {
+	let apiKey = "78238a72c5a21c61b3b1fb1a5792932d";
+	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric`;
+	axios.get(apiUrl).then(showTemperature);
 }
 
 let searchForm = document.querySelector("#searchForm");
 searchForm.addEventListener("submit", searchResult);
+
+function searchLocation(position) {
+	let apiKey = "78238a72c5a21c61b3b1fb1a5792932d";
+	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+	axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentLocation(event) {
+	event.preventDefault();
+	navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
 
 //Feature #3
 
