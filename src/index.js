f0b8c5635fc149d1165dd215b1e29951bb809a8c
theselenaliu <79890458+theselenaliu@users.noreply.github.com>
2021-03-20 22:44:24 -0700

    Added forecast APIs and changed CSS of entire app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 let now = new Date();
 let currentHour = now.getHours();
-let currentMinutes = now.getMinutes();
+let currentMinutes = String(now.getMinutes()).padStart(2, "0");
 let days = [
 	"Sunday",
 	"Monday",
@@ -32,16 +32,75 @@ function showTemperature(response) {
 	console.log("temp" + response.data.main.temp);
 	document.querySelector("#currentCity").innerHTML = response.data.name;
 	let temperature = Math.round(response.data.main.temp);
+	let weatherDescription = response.data.weather[0].description;
 	let temperatureElement = document.querySelector("#temperature");
+	let weatherDescriptionElement = document.querySelector(
+		"#weather-text-description"
+	);
+	let precipitationElement = document.querySelector("#precipitation");
+	let humidityElement = document.querySelector("#humidity");
+	let windElement = document.querySelector("#wind");
+	let iconElement = document.querySelector("#icon");
+
 	temperatureElement.innerHTML = `${temperature}`;
+	weatherDescriptionElement.innerHTML = weatherDescription;
+
+	windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+	humidityElement.innerHTML = response.data.main.humidity;
+	iconElement.setAttribute(
+		"src",
+		`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+	);
+	iconElement.setAttribute("alt", response.data.weather[0].description);
+
 	let celsiusTemp = Math.round(temperature);
 	let fahrenheitTemp = Math.round((temperature * 9) / 5 + 32);
+}
+
+function dispalyForecast(response) {
+	let forecastElement = document.querySelector("#forecast");
+	forecastElement.innerHTML = null;
+	let forecast = null;
+	let forecastTitle = document.querySelector("#forecast-title");
+	for (let index = 0; index < 6; index++) {
+		forecast = response.data.list[index];
+		forecastTitle.innerHTML = "3-Hourly Forecast";
+		forecastElement.innerHTML += `<div class="row"> <div class="col" id="forecast-time">${formatHours(
+			forecast.dt * 1000
+		)}</div>
+							<div class="col"> <img
+        src="http://openweathermap.org/img/wn/${
+					forecast.weather[0].icon
+				}@2x.png"
+      />
+        ${Math.round(forecast.main.temp_min)}°
+      </div></div>
+							
+	</div>
+  `;
+	}
+}
+function formatHours(timestamp) {
+	let date = new Date(timestamp);
+	let hours = date.getHours();
+	if (hours < 10) {
+		hours = `0${hours}`;
+	}
+	let minutes = date.getMinutes();
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
+
+	return `${hours}:${minutes}`;
 }
 
 function searchCity(cityInput) {
 	let apiKey = "78238a72c5a21c61b3b1fb1a5792932d";
 	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric`;
 	axios.get(apiUrl).then(showTemperature);
+
+	apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput.value}&appid=${apiKey}&units=metric`;
+	axios.get(apiUrl).then(dispalyForecast);
 }
 
 let searchForm = document.querySelector("#searchForm");
@@ -61,12 +120,6 @@ function getCurrentLocation(event) {
 
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
-
-//Feature #3
-
-let temperature = 25;
-let celsiusTemp = Math.round(temperature);
-let fahrenheitTemp = Math.round((temperature * 9) / 5 + 32);
 
 function convertToFahrenheit(event) {
 	event.preventDefault();
